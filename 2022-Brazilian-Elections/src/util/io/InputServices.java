9bfde23e1fe926541c5df5d9de5cc7a55d280a1b
Joao Batista <jpcbatista.2004@gmail.com>
2023-10-26 16:40:02 -0300

    🤵 First commit

---
+++ 2022-Brazilian-Elections/src/util/io/InputServices.java
@@ -0,0 +1,18 @@
+package util.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.nio.charset.Charset;
+
+public class InputServices {
+    public static BufferedReader createBufferedReader(String path) throws IOException{
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(path), Charset.forName("ISO-8859-1")));
+            return br;
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
