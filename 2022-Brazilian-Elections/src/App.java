3a3a52fd55e6c62ea7d8e55271464c5151724551
Joao Batista <jpcbatista.2004@gmail.com>
2023-10-28 19:26:41 -0300

    ⚒️ Adding build file (xml)

--- 2022-Brazilian-Elections/src/App.java
+++ 2022-Brazilian-Elections/src/App.java
@@ -1,3 +1,45 @@
+import java.io.BufferedReader;
+import java.time.LocalDate;
+
+import domain.Eleicao;
+import util.exceptions.ExceptionArquivoCandidatos;
+import util.exceptions.ExceptionArquivoPartidos;
+import util.exceptions.ExceptionDataEleicao;
+import util.io.InputServices;
+
 public class App {
-    
+    public static void main(String[] args) throws Exception{
+        int tipo;
+        if(args[0] == "--estadual"){
+            tipo = 6;
+        } else if(args[0] == "--federal"){
+            tipo = 7;
+        } else{
+            throw new Exception("Argumento inválido para o tipo de eleição!");
+        }
+
+        BufferedReader bufferCandidatos, bufferPartidos;
+        try{
+            bufferCandidatos = InputServices.createBufferedReader(args[1]);
+        } catch(Exception e){
+            throw new ExceptionArquivoCandidatos();
+        }
+        try{
+            bufferPartidos = InputServices.createBufferedReader(args[2]);
+        } catch(Exception e){
+            throw new ExceptionArquivoPartidos();
+        }
+
+        String[] data = args[3].split("/");
+        int dia, mes, ano;
+        try{
+            dia = Integer.parseInt(data[0]);
+            mes = Integer.parseInt(data[1]);
+            ano = Integer.parseInt(data[2]);
+        } catch(Exception e){
+            throw new ExceptionDataEleicao();
+        }
+
+        Eleicao eleicao = new Eleicao(tipo, LocalDate.of(ano, mes, dia));
+    }
 }
