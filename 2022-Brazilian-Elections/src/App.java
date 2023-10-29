21307695d413c25e169a9c5b31731b4f68b98099
Joao Batista <jpcbatista.2004@gmail.com>
2023-10-29 18:16:27 -0300

    📱 Adding reports methods to App

--- 2022-Brazilian-Elections/src/App.java
+++ 2022-Brazilian-Elections/src/App.java
@@ -1,31 +1,34 @@
 import java.io.BufferedReader;
 import java.time.LocalDate;
 
+import util.io.InputServices;
+import util.io.OutputServices;
+
 import domain.Eleicao;
+
 import util.exceptions.ExceptionArquivoCandidatos;
 import util.exceptions.ExceptionArquivoPartidos;
 import util.exceptions.ExceptionDataEleicao;
-import util.io.InputServices;
 
 public class App {
     public static void main(String[] args) throws Exception{
         int tipo;
         if(args[0].compareTo("--estadual") == 0){
-            tipo = 6;
-        } else if(args[0].compareTo("--federal") == 0){
             tipo = 7;
+        } else if(args[0].compareTo("--federal") == 0){
+            tipo = 6;
         } else{
             throw new Exception("Argumento inválido para o tipo de eleição!");
         }
 
-        BufferedReader bufferCandidatos, bufferPartidos;
+        BufferedReader bufferCandidatos, bufferVotacao;
         try{
             bufferCandidatos = InputServices.createBufferedReader(args[1]);
         } catch(Exception e){
             throw new ExceptionArquivoCandidatos();
         }
         try{
-            bufferPartidos = InputServices.createBufferedReader(args[2]);
+            bufferVotacao = InputServices.createBufferedReader(args[2]);
         } catch(Exception e){
             throw new ExceptionArquivoPartidos();
         }
@@ -44,6 +47,9 @@ public class App {
 
         try {
             InputServices.processarArquivoCandidatos(bufferCandidatos, eleicao);
+            InputServices.processarArquivoVotacao(bufferVotacao, eleicao);
+
+            OutputServices.geracaoDeRelatorios(eleicao);
         } catch (Exception e) {
             throw e;
         }
